28c0dd1a99bb924461c1ec7bd2a9b8b66ed438c8
Philip Niedertscheider <phil@techprimate.com>
2025-07-21 14:18:32 +0200

    feat: add emoji keyboard type extension

---
+++ Sources/Utils/UIKeyboardType+Emoji.swift
@@ -0,0 +1,6 @@
+import UIKit
+
+extension UIKeyboardType {
+    /// Source: https://www.reddit.com/r/SwiftUI/comments/1i2kfco/comment/m7j64vp/
+    static let emoji = UIKeyboardType(rawValue: 124)
+}
