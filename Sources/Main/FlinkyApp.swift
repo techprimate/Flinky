ead1320e8f6f149cae56ca5981fbd48540d6573e
Philip Niedertscheider <phil@techprimate.com>
2025-07-25 16:06:55 +0200

    feat: Enable all features of Sentry

--- Sources/Main/FlinkyApp.swift
+++ Sources/Main/FlinkyApp.swift
@@ -6,7 +6,7 @@ import OnLaunch
 @main
 struct FlinkyApp: App {
     @StateObject private var toastManager = ToastManager()
-    
+
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
             LinkListModel.self,
@@ -22,19 +22,202 @@ struct FlinkyApp: App {
     }()
 
     init() {
-        #if DEBUG
-            print("Sentry is disabled in debug mode")
-        #else
-            SentrySDK.start { options in
-                options.dsn = "https://f371822cfa840de0c6a27a788a5fa48e@o188824.ingest.us.sentry.io/4509640637349888"
+        SentrySDK.start { options in
+            Self.configureSentry(options: options)
+        }
+    }
 
-                options.sendDefaultPii = true
-                options.tracesSampleRate = 1.0
+    /// Configures the Sentry SDK options.
+    ///
+    /// This method is defined as `private static` to because it is called from a non-mutating context.
+    ///
+    /// - Parameter options: Options structure to configure Sentry.
+    private static func configureSentry(options: Options) {
+        options.dsn = "https://f371822cfa840de0c6a27a788a5fa48e@o188824.ingest.us.sentry.io/4509640637349888"
 
-                options.sessionReplay.onErrorSampleRate = 1.0
-                options.sessionReplay.sessionSampleRate = 0.1
+        options.releaseName = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
+        options.dist = Bundle.main.infoDictionary?["CFBundleVersion"] as? String
+#if DEBUG
+        options.environment = "development"
+#else
+        options.environment = "production"
+#endif
+
+        options.sampleRate = 1.0
+        options.tracesSampleRate = 1.0
+
+        // Configure General Options
+        options.sendDefaultPii = true
+        options.enableAutoBreadcrumbTracking = true
+        options.enableMetricKit = true
+        options.enableTimeToFullDisplayTracing = true
+        options.enableSwizzling = true
+        options.swiftAsyncStacktraces = true
+
+        // Configure Crash Reporting
+        options.enableCrashHandler = true
+        options.enableSigtermReporting = true
+        options.enableWatchdogTerminationTracking = true
+        options.attachStacktrace = true
+        options.enablePersistingTracesWhenCrashing = true
+
+        // Configure Profiling
+        options.enableAppLaunchProfiling = true
+        options.profilesSampleRate = 1.0
+
+        // Configure Session Replay
+        options.sessionReplay.onErrorSampleRate = 1.0
+        options.sessionReplay.sessionSampleRate = 1.0
+        options.sessionReplay.enableViewRendererV2 = true
+        options.sessionReplay.enableFastViewRendering = false
+
+        // Configure App Hang
+        options.enableAppHangTracking = true
+        options.enableAppHangTrackingV2 = true
+        options.enableReportNonFullyBlockingAppHangs = false
+
+        // Configure File I/O
+        options.enableFileIOTracing = true
+        options.experimental.enableDataSwizzling = true
+        options.experimental.enableFileManagerSwizzling = true
+
+        // Configure Tracing
+        options.enableAutoPerformanceTracing = true
+        options.enablePerformanceV2 = true
+        options.enableCoreDataTracing = true
+        options.enablePreWarmedAppStartTracing = true
+
+        // Configure UI Insights
+        options.enableUIViewControllerTracing = true
+        options.attachScreenshot = true
+        options.attachViewHierarchy = true
+        options.enableUserInteractionTracing = true
+        options.reportAccessibilityIdentifier = true
+
+        // Configure Networking
+        options.enableNetworkTracking = true
+        options.enableNetworkBreadcrumbs = true
+        options.enableGraphQLOperationTracking = true
+        options.enableCaptureFailedRequests = true
+
+        // Configure User Feedback
+        options.configureUserFeedback = { feedbackOptions in
+            feedbackOptions.animations = true
+            feedbackOptions.configureWidget = { widgetOptions in
+                widgetOptions.autoInject = true
+                widgetOptions.labelText = "Give Feedback"
+                widgetOptions.showIcon = true
+                widgetOptions.widgetAccessibilityLabel = "Feedback Widget"
+                widgetOptions.windowLevel = UIWindow.Level.normal + 1
+                widgetOptions.location = [.bottom, .trailing]
+                widgetOptions.layoutUIOffset = .init()
             }
-        #endif
+            feedbackOptions.showFormForScreenshots = true
+            feedbackOptions.configureForm = { formOptions in
+                formOptions.useSentryUser = true
+                formOptions.showBranding = true
+                formOptions.formTitle = "Give Feedback"
+                formOptions.messageLabel = "Description"
+                formOptions.messagePlaceholder = "Please describe the issue you encountered"
+                formOptions.messageTextViewAccessibilityLabel = "Feedback Message"
+                formOptions.isRequiredLabel = "(Required)"
+                formOptions.removeScreenshotButtonLabel = "Remove Screenshot"
+                formOptions.removeScreenshotButtonAccessibilityLabel = "Remove Screenshot"
+                formOptions.isNameRequired = false
+                formOptions.showName = false
+                formOptions.nameLabel = "Name"
+                formOptions.namePlaceholder = "Your Name"
+                formOptions.nameTextFieldAccessibilityLabel = "Your Name"
+
+                formOptions.isEmailRequired = false
+                formOptions.showEmail = true
+                formOptions.emailLabel = "Email"
+                formOptions.emailPlaceholder = "Your Email"
+                formOptions.emailTextFieldAccessibilityLabel = "Your Email"
+
+                formOptions.submitButtonLabel = "Send Feedback"
+                formOptions.submitButtonAccessibilityLabel = "Send Feedback"
+
+                formOptions.cancelButtonLabel = "Cancel"
+                formOptions.cancelButtonAccessibilityLabel = "Cancel Feedback"
+            }
+            feedbackOptions.configureTheme = { themeOptions in
+                // The font family to use for form text elements.
+                themeOptions.fontFamily = nil // Defaults to system font
+
+                // Foreground text color of the widget and form.
+                themeOptions.foreground = UIColor.label
+
+                // Background color of the widget and form.
+                themeOptions.background = UIColor.systemBackground
+
+                // Foreground color for the form submit button.
+                themeOptions.submitForeground = UIColor.systemBlue
+
+                // Background color for the form submit button in light and dark modes.
+                themeOptions.submitBackground = UIColor.systemBackground
+
+                // Foreground color for the cancel and screenshot buttons.
+                themeOptions.buttonForeground = UIColor.label
+
+                // Background color for the form cancel and screenshot buttons in light and dark modes.
+                themeOptions.buttonBackground = UIColor.systemBackground
+
+                // Color used for error-related components (such as text color when there's an error submitting feedback).
+                themeOptions.errorColor = UIColor.systemRed
+
+                // Options for styling the outline of input elements and buttons in the feedback form.
+                themeOptions.outlineStyle.color = UIColor.systemGray
+                themeOptions.outlineStyle.cornerRadius = 5
+                themeOptions.outlineStyle.outlineWidth = 0.5
+            }
+            feedbackOptions.configureDarkTheme = { themeOptions in
+                // Background color to use for text inputs in the feedback form.
+                themeOptions.inputBackground = UIColor.systemGray6
+
+                // Background color to use for text inputs in the feedback form.
+                themeOptions.inputForeground = UIColor.label
+
+                // The font family to use for form text elements.
+                themeOptions.fontFamily = nil // Defaults to system font
+
+                // Foreground text color of the widget and form.
+                themeOptions.foreground = UIColor.label
+
+                // Background color of the widget and form.
+                themeOptions.background = UIColor.systemBackground
+
+                // Foreground color for the form submit button.
+                themeOptions.submitForeground = UIColor.systemBlue
+
+                // Background color for the form submit button in light and dark modes.
+                themeOptions.submitBackground = UIColor.systemBackground
+
+                // Foreground color for the cancel and screenshot buttons.
+                themeOptions.buttonForeground = UIColor.label
+
+                // Background color for the form cancel and screenshot buttons in light and dark modes.
+                themeOptions.buttonBackground = UIColor.systemBackground
+
+                // Color used for error-related components (such as text color when there's an error submitting feedback).
+                themeOptions.errorColor = UIColor.systemRed
+
+                // Options for styling the outline of input elements and buttons in the feedback form.
+                themeOptions.outlineStyle.color = UIColor.systemGray
+                themeOptions.outlineStyle.cornerRadius = 5
+                themeOptions.outlineStyle.outlineWidth = 0.5
+            }
+
+            feedbackOptions.onFormOpen = {
+                // Custom logic when the feedback form is opened
+            }
+            feedbackOptions.onFormClose = {
+                // Custom logic when the feedback form is closed
+            }
+        }
+
+        // Configure Other Options
+        options.experimental.enableUnhandledCPPExceptionsV2 = false
     }
 
     var body: some Scene {
