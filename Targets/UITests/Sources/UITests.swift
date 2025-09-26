9896cfb512ccebd205d947663f7a011608a3bf29
Philip Niedertscheider <phil@techprimate.com>
2025-09-26 11:50:47 +0200

    chore: Refactor build system and add new test schemes (#36)
    - Updated .gitignore to include new log files for iOS app and share extension builds.
    - Refactored Makefile to separate build targets for app, core, and share extension.
    - Added new test schemes for FlinkyCore and ShareExtension, including configurations for UI and unit tests.
    - Created new test plans for various test targets to streamline testing processes.
    - Introduced new test files for persistence and model tests in FlinkyCore.
    - Ensured all new test targets are properly configured in the Xcode project.

---
+++ Targets/UITests/Sources/UITests.swift
@@ -0,0 +1,16 @@
+import XCTest
+
+final class UITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunchPerformance() throws {
+        measure(metrics: [XCTApplicationLaunchMetric()]) {
+            XCUIApplication().launch()
+        }
+    }
+}
